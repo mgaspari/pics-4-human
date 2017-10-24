d9698f432db0a45221e2032c8f280c936e6098f7
github username <github email address>
2017-10-24 09:48:41 -0400

    This better work

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default class Scoreboard extends React.Component{
+  render(){
+    return(
+      <div class="ui vertical menu">
+        <a class="item">
+          <div class="ui teal label">1</div></a>
+        <a class="item">
+          <div class="ui teal label">51</div>Spam</a>
+        <a class="item">
+          <div class="ui teal label">1</div>Updates</a>
+        <a class="item">
+          <div class="ui teal label">1</div>Updates</a>
+      </div>
+    )
+  }
+}
